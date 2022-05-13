a9ad99ec687c7c89fea32d42601a3f2e3bd43c3d
proginoskes <proginoskes@vivaldi.net>
2022-05-12 23:46:14 -0400

    completes linear congruential random

--- mathematical/03_randomnums/linear_congruential.cpp
+++ mathematical/03_randomnums/linear_congruential.cpp
@@ -11,11 +11,8 @@
 // we can only represent ints less than 2^31
 // so any terms of multiplication must be
 // 2^15 or less
-int safe_mul(int p, int q, int max){
-    int p1, p0, q1, q0, m1 = max;
-    while(m1 > 32768){
-        m1 = m1 >> 1;
-    }
+int safe_mul(int p, int q, int m1){
+    int p1, p0, q1, q0;
     p1 = p / m1; p0 = p % m1;
     q1 = q / m1; q0 = q % m1;
     return (((p0*q1+p1*q0)%m1)*m1+ p0*q0);
@@ -41,8 +38,8 @@ int * unsafe_lin_generate_seq(int seed, int max, int n){
     return a;
 }
 
-int * safe_lin_generate_seq(int seed, int max, int n){
-    int iter, *a, cfact;
+int * better_safe_lin_generate_seq(int seed, int range, int max, int n){
+    int iter, *a, cfact, m1=max;
     a = (int *)malloc(n * sizeof(int));
     a[0] = seed;
     // a number with one less digit than m
@@ -53,8 +50,40 @@ int * safe_lin_generate_seq(int seed, int max, int n){
     cfact += 621;
     std::cout << "b:" << cfact<<"\n";
 
+    while(m1 > 32768){
+        m1 = m1 >> 1;
+    }
+
     for(iter=1; iter<n; iter++){
-        a[iter] = ((safe_mul(a[iter-1],cfact, max)%max) + 1)%max;
+        a[iter] = ((safe_mul(a[iter-1],cfact, m1)%max) + 1)%max;
+        // this version uses floats to get the ratio of
+        // generated values to max and then multiplies by
+        // a top-range value to remove the "counting up"
+        // problem from before.
+        // 
+        a[iter] = (float(a[iter])/float(max))*float(range);
+    }
+    return a;
+}
+
+int * safe_lin_generate_seq(int seed, int max, int n){
+    int iter, *a, cfact, m1=max;
+    a = (int *)malloc(n * sizeof(int));
+    a[0] = seed;
+    // a number with one less digit than m
+    cfact = max/10;
+    // and also that ends in x21 with x even
+    cfact /= 1000;
+    cfact *= 1000;
+    cfact += 621;
+    std::cout << "b:" << cfact<<"\n";
+
+    while(m1 > 32768){
+        m1 = m1 >> 1;
+    }
+
+    for(iter=1; iter<n; iter++){
+        a[iter] = ((safe_mul(a[iter-1],cfact, m1)%max) + 1)%max;
     }
     return a;
 }
@@ -62,7 +91,7 @@ int * safe_lin_generate_seq(int seed, int max, int n){
 
 // argv: ./a.out seed max n mode
 int main(int argc, char **argv){
-    int *a, n, max, seed, iter, mode;
+    int *a, n, max, seed, iter, mode, range=0;
 
     seed = std::stoi(argv[1]);
     max = std::stoi(argv[2]);
@@ -70,6 +99,10 @@ int main(int argc, char **argv){
     mode = std::stoi(argv[4]);
     
     switch(mode){
+       case 1:
+            range = std::stoi(argv[5]);
+            a = better_safe_lin_generate_seq(seed,range,max,n);
+            break;
        case 0:
             a = safe_lin_generate_seq(seed,max,n);
             break;
